165dec935f8be7064fc3bd089e80a3c653179e4e
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2022-05-07 05:32:47 +0100

    Fix some bugs

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -8,52 +8,68 @@ import { Spinner } from '../Spinner'
 import { useSelector } from '../../store'
 import { filterChains, isArrayNotEmpty } from '../../utils/data'
 import { chainService } from '@src/services/chain'
+import Image from 'next/image'
 
 const tableHeadings = ['Icon', 'Name', 'Token Symbols', 'Status']
 const DURATION = 5 * 60000
+
+const TableRow = ({ row }) => {
+    const { icon, name, tokenSymbols, connected } = row
+    const [status, setStatus] = useState(connected)
+
+    useEffect(() => {
+        setInterval(async () => {
+            try {
+                const response = await chainService.getNetworkStatus(name?.toLowerCase())
+                const networkStatus = await (response?.data)
+                setStatus(networkStatus)
+            } catch (error) {
+                console.log(error)
+            }
+        }, DURATION)
+
+        return () => {
+            clearInterval()
+        }
+    })
+
+
+    return (
+        <tr className={styles.table__row}>
+            <td className={styles.table__cell}>
+                <Image src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/images/${icon}`}
+                    alt={icon}
+                    width={50}
+                    height={50}
+                />
+            </td>
+            <td className={styles.table__cell}>{name}</td>
+            <td className={styles.table__cell}>{tokenSymbols[0]}</td>
+
+            <td className={styles.table__cell}>
+                <div className="flex">
+                    <span className={`status ${status ? 'status__green' : 'status__red'}`} ></span>
+                    {status ? 'Connected' : 'Not connected'}
+                </div>
+            </td>
+        </tr>
+    )
+}
 
 const TableBody = () => {
     const allChains = useSelector(state => state.chains.allchainNetworks)
     const filteredChains = filterChains(allChains)
     const isAllChainsNotEmpty = isArrayNotEmpty(filteredChains)
+
+
     return (
         <>
 
             {
-                isAllChainsNotEmpty ? filteredChains.map(({ icon, name, tokenSymbols, connected }, index) => {
-                    let status = connected
-                    useEffect(() => {
-                        setInterval(async () => {
-                            try {
-                                const response = await chainService.getNetworkStatus(name.toLowerCase())
-                                status = await response?.data
-                                console.log(`${name}: `, status)
-                            } catch (error) {
-                                console.log(error)
-                            }
-                            console.log('hey man!')
-                        }, DURATION)
-
-                        return () => {
-                            clearInterval()
-                        }
-                    }, [DURATION])
+                isAllChainsNotEmpty ? filteredChains.map((row, index) => {
 
                     return (
-                        <tr className={styles.table__row} key={index}>
-                            <td className={styles.table__cell}>
-                                <img src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/images/${icon}`} alt={icon} className={styles.table__img} />
-                            </td>
-                            <td className={styles.table__cell}>{name}</td>
-                            <td className={styles.table__cell}>{tokenSymbols[0]}</td>
-
-                            <td className={styles.table__cell}>
-                                <div className="flex">
-                                    <span className={`status ${status ? 'status__green' : 'status__red'}`} ></span>
-                                    {status ? 'Connected' : 'Not connected'}
-                                </div>
-                            </td>
-                        </tr>
+                        <TableRow row={row} key={index} />
                     )
                 }) : null
             }
@@ -68,12 +84,12 @@ const HomeContent = () => {
     const fetchChains = useCallback(() => {
         setIsFetching(true)
         dispatch(action(ChainStatusTypes.GET_ALL_NETWORK_CHAIN_REQUEST, onSuccess, onError))
-    }, [])
+    }, [dispatch])
 
 
     useEffect(() => {
         fetchChains()
-    }, [])
+    }, [fetchChains])
 
 
     const onSuccess = () => {
